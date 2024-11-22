b947557459d825b176cfcbbd8eb8465ff61014fc
utilisateur <utilisateur@MSI>
2024-11-22 13:48:35 +0100

    Add project files.

---
+++ MauiAppinleveropdracht/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiAppinleveropdracht
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInButton_Clicked(object sender, EventArgs e)
+        {
+            bool IsUsernameEmpty = string.IsNullOrEmpty(UsernameEntry.Text);
+            bool IsPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if (IsUsernameEmpty)
+            {
+                UsernameEntry.Placeholder = "Vul iets in!";
+            }
+
+            else if (IsPasswordEmpty)
+            {
+                PasswordEntry.Placeholder = "Vul iets in!";
+            }
+
+            else
+            {
+                Navigation.PushAsync(new NewPage1());
+            }
+
+        }
+    }
+
+
+}
