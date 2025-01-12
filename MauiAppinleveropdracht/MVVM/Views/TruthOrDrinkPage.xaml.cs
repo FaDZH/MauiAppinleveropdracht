34dfea602555944711cd6e3dec37017f8e1348b3
Fatah Dzhamaldinov <2305175dzhamaldinov@zuyd.nl>
2025-01-12 23:32:27 +0100

    tril functie toegevoegd voor te vaak drinken

--- MauiAppinleveropdracht/MVVM/Views/TruthOrDrinkPage.xaml.cs
+++ MauiAppinleveropdracht/MVVM/Views/TruthOrDrinkPage.xaml.cs
@@ -1,41 +1,66 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using Microsoft.Maui.Devices;
 
-namespace MauiAppinleveropdracht
+namespace MauiAppinleveropdracht;
+
+public partial class TruthOrDrinkPage : ContentPage
 {
-    public partial class TruthOrDrinkPage : ContentPage
+    private readonly TruthOrDrinkGame _game;
+    private readonly List<string> _players;
+    private int _currentPlayerIndex;
+    private int _drinkCount; 
+
+    public TruthOrDrinkPage(List<string> players, string theme)
     {
-        private readonly TruthOrDrinkGame _game;
-        private readonly List<string> _players;
-        private int _currentPlayerIndex;
+        InitializeComponent();
+        _players = players;
+        _game = new TruthOrDrinkGame(theme);
+        _currentPlayerIndex = 0;
+        _drinkCount = 0; 
 
-        public TruthOrDrinkPage(List<string> players, string theme)
-        {
-            InitializeComponent();
-            _players = players;
-            _game = new TruthOrDrinkGame(theme);
-            _currentPlayerIndex = 0;
-
-            _ = LoadNextQuestionAsync();
-        }
-
-        private async Task LoadNextQuestionAsync()
-        {
-            string currentPlayer = _players[_currentPlayerIndex];
-            string nextQuestion = await _game.GetNextQuestionAsync();
-
-            QuestionLabel.Text = $"{currentPlayer}'s beurt: {nextQuestion}";
-            _currentPlayerIndex = (_currentPlayerIndex + 1) % _players.Count;
-        }
-
-        private async void OnTruthClicked(object sender, EventArgs e)
-        {
-            await LoadNextQuestionAsync();
-        }
-
-        private async void OnDrinkClicked(object sender, EventArgs e)
-        {
-            await LoadNextQuestionAsync();
-        }
+        _ = LoadNextQuestionAsync();
     }
+
+    private async Task LoadNextQuestionAsync()
+    {
+        string currentPlayer = _players[_currentPlayerIndex];
+        string nextQuestion = await _game.GetNextQuestionAsync();
+
+        QuestionLabel.Text = $"{currentPlayer}'s beurt: {nextQuestion}";
+        _currentPlayerIndex = (_currentPlayerIndex + 1) % _players.Count;
+    }
+
+    private async void OnTruthClicked(object sender, EventArgs e)
+    {
+        await LoadNextQuestionAsync();
+    }
+
+    private async void OnDrinkClicked(object sender, EventArgs e)
+    {
+        _drinkCount++; 
+
+        if (_drinkCount >= 5) 
+        {
+            ShowDrinkWarning(); 
+            _drinkCount = 0;    
+        }
+
+        await LoadNextQuestionAsync();
+    }
+
+    private async void ShowDrinkWarning()
+    {
+        if (Vibration.Default.IsSupported)
+        {
+            Console.WriteLine("Vibratie gestart voor 1 seconde."); //als dit bericht in console komt dan heb ik een indicatie dat het functie is gestart.
+            Vibration.Default.Vibrate(TimeSpan.FromSeconds(1));
+        }
+        else
+        {
+            Console.WriteLine("Vibratie wordt niet ondersteund op dit apparaat."); //als ik het zonder dit deed ging mijn emulator steeds crashen dus ja.
+        }
+
+        await DisplayAlert("Rustig aan!", "Je hebt te vaak achter elkaar op 'Drink' geklikt. Neem een pauze!", "OK");
+    }
+
 }
