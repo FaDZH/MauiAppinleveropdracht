c00a1352cb27e6db1da2515ab98da4baaf71f9ab
Fatah Dzhamaldinov <2305175dzhamaldinov@zuyd.nl>
2025-01-13 02:49:16 +0100

    inlog

--- MauiAppinleveropdracht/MVVM/Views/MainPage.xaml.cs
+++ MauiAppinleveropdracht/MVVM/Views/MainPage.xaml.cs
@@ -1,47 +1,74 @@
-﻿namespace MauiAppinleveropdracht
+﻿using Microsoft.Maui.Storage;
+
+namespace MauiAppinleveropdracht;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        public MainPage()
+        InitializeComponent();
+
+        CheckSavedCredentials();
+    }
+
+    private async void CheckSavedCredentials()
+    {
+        var savedUsername = await SecureStorage.Default.GetAsync("username");
+        var savedPassword = await SecureStorage.Default.GetAsync("password");
+
+        if (!string.IsNullOrEmpty(savedUsername) && !string.IsNullOrEmpty(savedPassword))
         {
-            InitializeComponent();
-        }
-
-        private async void LogInButton_Clicked(object sender, EventArgs e)
-        {
-            bool isUsernameEmpty = string.IsNullOrEmpty(UsernameEntry.Text);
-            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
-
-            if (isUsernameEmpty || isPasswordEmpty)
-            {
-                await DisplayAlert("Fout", "Vul alle velden in.", "OK");
-                return;
-            }
-
             using (var db = new SQLite.SQLiteConnection(DBConstants.DatabasePath, DBConstants.Flags))
             {
                 db.CreateTable<User>();
                 var user = db.Table<User>()
-                             .FirstOrDefault(u => u.Username == UsernameEntry.Text && u.Password == PasswordEntry.Text);
+                             .FirstOrDefault(u => u.Username == savedUsername && u.Password == savedPassword);
 
                 if (user != null)
                 {
-                    // Hier slaat die ingelogde naam op
                     App.CurrentUserName = user.Username;
 
                     await Navigation.PushAsync(new Spelmenu(App.CurrentUserName));
                 }
-                else
-                {
-                    await DisplayAlert("Fout", "Ongeldige gebruikersnaam of wachtwoord.", "OK");
-                }
             }
-
         }
+    }
 
-        private async void RegisterButton_Clicked(object sender, EventArgs e)
+    private async void LogInButton_Clicked(object sender, EventArgs e)
+    {
+        bool isUsernameEmpty = string.IsNullOrEmpty(UsernameEntry.Text);
+        bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+        if (isUsernameEmpty || isPasswordEmpty)
         {
-            await Navigation.PushAsync(new RegisterPage());
+            await DisplayAlert("Fout", "Vul alle velden in.", "OK");
+            return;
         }
+
+        using (var db = new SQLite.SQLiteConnection(DBConstants.DatabasePath, DBConstants.Flags))
+        {
+            db.CreateTable<User>();
+            var user = db.Table<User>()
+                         .FirstOrDefault(u => u.Username == UsernameEntry.Text && u.Password == PasswordEntry.Text);
+
+            if (user != null)
+            {
+                App.CurrentUserName = user.Username;
+
+                await SecureStorage.Default.SetAsync("username", UsernameEntry.Text);
+                await SecureStorage.Default.SetAsync("password", PasswordEntry.Text);
+
+                await Navigation.PushAsync(new Spelmenu(App.CurrentUserName));
+            }
+            else
+            {
+                await DisplayAlert("Fout", "Ongeldige gebruikersnaam of wachtwoord.", "OK");
+            }
+        }
+    }
+
+    private async void RegisterButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new RegisterPage());
     }
 }
